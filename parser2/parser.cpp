5c5de943f96143bb543c017b2b25a494390aedff
Michael Ganger <mganger747@gmail.com>
2014-10-11 17:13:58 -0400

    got the quadmath to woork correctly. Talk about precision

--- parser2/parser.cpp
+++ parser2/parser.cpp
@@ -1,10 +1,12 @@
 //this program converts the table of values to wavelength
 
-#include <math.h>
+#include <cmath>
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <string>
+#include <quadmath.h>
+
 
 using namespace std;
 
@@ -75,22 +77,82 @@ class DataPoint : protected Calibration, public Wavelength {
 		double setPower(double input){power = input;}
 };
 
+class StefanBoltzmann {
+	public:
+		static constexpr double sigma = 5.67e-8;
+		static double intensity(double temp){return sigma * pow(temp, 4);}
+};
+
+class WiensLaw {
+	private:
+		double wConst = 2.89776829;
+		double temperature;
+		double wavelength;
+
+	public:
+		WiensLaw(){};
+		WiensLaw(double temp){
+			temperature = temp;
+			wavelength = wConst / temp;
+		}
+		double getTemp(){return temperature;}
+		double getWavelength(){return wavelength;}
+
+};
+
+class PowerDensity {
+	private:
+		__float128 pi = M_PI;
+		__float128 h = 6.626e-34L;
+		__float128 k = 5.67e-8L;
+		__float128 c = 3e8L;
+
+		long double wavelength;
+		long double temperature;
+		long double density;
+
+		long double densityCalc(){
+			__float128 num, denum;
+			num = 2 * pi * h * c*c;
+			denum = powq(wavelength, 5) * (expq((h*c) / (wavelength * k * temperature)) - 1);
+			cout << "num:   " << static_cast<long double>(num) << endl;
+			cout << "denum: " << static_cast<long double>(denum) << endl;
+			return num / denum;
+		}
+	public:
+		PowerDensity(long double inputWavelength, long double inputTemperature){
+			wavelength = inputWavelength * 1e-9;
+			temperature= inputTemperature;
+			density = densityCalc();
+		}
+
+		long double getWavelength(){return wavelength * 1e9;}
+		double getTemp(){return temperature;}
+		long double getDensity(){return density * 1e9;}
+};
+
+
+
+
 //this class handles the specific calibration of temperature data
 class TempCalibration {
 	private:
 		double maximum;
 		double baseline;
-		double maxPower;
+		long double maxPower;
+		double maxWavelength;
 
 	public:
-		TempCalibration(vector<DataPoint>& input, int sampleSize, double timeRange, double targetPower){
+		TempCalibration(vector<DataPoint>& input, int sampleSize, double timeRange, double temperature){
 			maximum = maxDataPoint(input, sampleSize);
 			baseline = findBaseline(input, timeRange);
-			maxPower = targetPower;
-			cout << targetPower * 2 << " point moving average" << endl;
+			PowerDensity maxDensity(maxWavelength, temperature);
+			maxPower = maxDensity.getDensity();
+
+			cout << sampleSize * 2 << " point moving average" << endl;
 			cout << "Max:    " << maximum << " V" << endl;
 			cout << "base:   " << baseline << " V" << endl;
-			cout << "target: " << targetPower << " W/m" << endl;
+			cout << "target: " << maxPower << " W/m^2 / nm" << endl;
 		}
 
 		//needs to get the baseline for the measurement, and then find the max temperature
@@ -112,6 +174,7 @@ class TempCalibration {
 					peak = input[i].getWavelength();
 				}
 			}
+			maxWavelength = peak;
 			cout << "Peak Wavelength: " << peak << " nm" << endl;
 			return max;
 		}
@@ -193,6 +256,51 @@ class InputFile {
 
 
 
+//uses information about the light source to obtain the right blackbody spectrum
+class PowerOutput {
+	private:
+		WiensLaw prediction;
+		double voltage;
+		double current;
+		double hotResistance;
+		double coldResistance;
+		double a0 = 4.5e-3;
+		double sigma = 5.67e-8;
+
+		double temp0;
+		double temperature;
+		double intensity;
+
+		double tempCalc(){
+			double resistRatio = hotResistance / coldResistance;
+			return temp0 + (resistRatio - 1) / a0;
+		}
+
+	public:
+		PowerOutput(double resistance, double startingTemp, double inputVoltage, double inputCurrent){
+			voltage = inputVoltage;
+			current = inputCurrent;
+			coldResistance = resistance;
+			hotResistance = voltage / current;
+			temp0 = startingTemp;
+
+			temperature = tempCalc();
+			prediction = WiensLaw(temperature);
+			intensity = StefanBoltzmann::intensity(temperature);
+		}
+
+		double getTemp(){return temperature;}
+		double getTemp0(){return temp0;}
+		double getHotResist(){return hotResistance;}
+		double getColdResist(){return coldResistance;}
+		double getVoltage(){return voltage;}
+		double getCurrent(){return current;}
+		double getWavelength(){return prediction.getWavelength();}
+		double getIntensity(){return intensity;}
+};
+
+
+
 vector<string> toStringArray(int argc, char ** argv){
 	vector<string> output;
 	for(int i = 1; i < argc; i++){
@@ -202,11 +310,12 @@ vector<string> toStringArray(int argc, char ** argv){
 }
 
 
+
 int main(int argc, char ** argv){
 	vector<string> args = toStringArray(argc, argv);
-	if(args.size() != 5){
+	if(args.size() != 8){
 		cout << "Wrong number of arguments" << endl;
-		cout << "input output samples timeRange targetPower" << endl;
+		cout << "input output samples timeRange voltage current resistance temperature" << endl;
 		exit(1);
 	}
 
@@ -219,17 +328,33 @@ int main(int argc, char ** argv){
 	ofstream outputFile(args[1], ios::trunc);
 	vector<DataPoint> data = rawFile.getPointArray();
 
+	//find the WiensLaw stuff
+	double voltage = atof(args[4].c_str());
+	double current = atof(args[5].c_str());
+	double resistance = atof(args[6].c_str());
+	double temperature = atof(args[7].c_str());
+	PowerOutput power(resistance, temperature, voltage, current);
+
+	cout << "Temp:       " << power.getTemp() << endl;
+	cout << "Temp0:      " << power.getTemp0() << endl;
+	cout << "HotResist:  " << power.getHotResist() << endl;
+	cout << "ColdResist: " << power.getColdResist() << endl;
+	cout << "Voltage:    " << power.getVoltage() << endl;
+	cout << "Current:    " << power.getCurrent() << endl;
+	cout << "Wavelength: " << power.getWavelength() << endl;
+	cout << "Intensity:  " << power.getIntensity() << endl << endl;
+
 	//calibrate points
 	int sampleSize = atoi(args[2].c_str());
 	if(sampleSize <=0) sampleSize = 1;
 	double timeRange = atof(args[3].c_str());
-	double targetPower = atof(args[4].c_str());
+	double targetPower = power.getIntensity();
 	TempCalibration tempCal(data, sampleSize, timeRange, targetPower);
 	tempCal.calibrateDataPoints(data);
 
 	//output the file
 	cout << endl << "Outputting to file in form:" << endl;
-	cout << "Time(s); Voltage(V); CalibratedAngle(°); Wavelength(nm); Intensity(W/m)" << endl;
+	cout << "Time(s); Voltage(V); CalibratedAngle(°); Wavelength(nm); Intensity(W/m^2 / nm)" << endl;
 	int size = data.size();
 	for(int j = 0; j < size; j++){
 		outputFile << data[j].getTime() << ';';
@@ -238,4 +363,8 @@ int main(int argc, char ** argv){
 		outputFile << data[j].getWavelength() << ';';
 		outputFile << data[j].getPower() << endl;
 	}
+
+	//test
+	cout << "=================" << endl;
+	PowerDensity test(330, 2500);
 }
