1774ccabeb4eda9fd6d09e878c1cae59ee5ef77c
Michael Ganger <mganger747@gmail.com>
2014-10-15 18:50:15 -0400

    Added a stats library. It inherits from the vector library, so it is easy to add stuff to it

---
+++ parser2/Stats.hpp
@@ -0,0 +1,34 @@
+//this header is supposed to simplify standard deviations
+
+#include "Measurement.hpp"
+#include <vector>
+
+template<class T>
+class Mean : public std::vector<T> {
+
+	public:
+		T mean(){
+			int vectorSize = this->size();
+			T sum = 0;
+			for(int i = 0; i < vectorSize; i++){
+				sum += (*this)[i];
+			}
+			return sum / vectorSize;
+		}
+		T variance(){
+			int vectorSize = this->size();
+			T sum = 0;
+			T mean = this->mean();
+			for(int i = 0; i < vectorSize; i++){
+				T inner = (*this)[i] - mean;
+				sum += inner * inner;
+			}
+			return sum / vectorSize;
+		}
+		T stdDev(){return sqrt(this->variance());}
+		T uncertainty(){return sqrt(this->variance() / this->size());};
+
+		Measurement<T> measurement(){
+			return Measurement<T>(this->mean(), this->uncertainty());
+		}
+};
---
+++ parser2/statsTest.cpp
@@ -0,0 +1,18 @@
+
+#include "Measurement.hpp"
+#include "Stats.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	Mean<double> test;
+	for(int i = 0; i < 100; i++) test.push_back(i);
+
+	cout << "Mean:        " << test.mean() << endl;
+	cout << "Variance:    " << test.variance() << endl;
+	cout << "Std Dev:     " << test.stdDev() << endl;
+	cout << "Uncertainty: " << test.uncertainty() << endl;
+	cout << "measurement: " << test.measurement() << endl;
+}
