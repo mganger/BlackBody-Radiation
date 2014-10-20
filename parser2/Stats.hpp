01251318dd708d73f97d0707410769ac5582b3d8
Michael Ganger <mganger747@gmail.com>
2014-10-20 19:17:13 -0400

    Moved integratioon to the stats library and made some tests for it

--- parser2/Stats.hpp
+++ parser2/Stats.hpp
@@ -4,6 +4,11 @@
 
 #include "Measurement-Type/Measurement.hpp"
 #include <vector>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
+
+using std::cout;
+using std::endl;
 
 template<class T>
 class Mean : public std::vector<T> {
@@ -46,6 +51,54 @@ class Trap {
 			T diff = b1 - b2;
 			error = h * sqrt(diff * diff);
 		}
+};
+
+//note that the template class must implement independent() and dependent()
+template <class T>
+bool integrationCompare(T lhs, T rhs){return lhs.independent() < rhs.independent();}
+
+//I = input, T = output
+template <class I, class T>
+class Integrate {
+	private:
+		std::vector<T> integrated;
+		std::vector<I> * inputData;
+		T offset;
+		T max; //V*nm
+
+	public:
+		Integrate(std::vector<I>& input, T inputOffset){
+			std::sort(input.begin(), input.end(), integrationCompare<I>);
+			inputData = &input;
+			integrated = std::vector<T>(input.size(), 0);
+			offset = inputOffset;
+		}
+
+		void integration(){
+			auto ot = integrated.begin();
+			ot++;
+
+			for(auto it = inputData->begin(); it != (inputData->end() - 1); it++){
+				T h = it[1].independent() - it[0].independent();
+
+				if(boost::math::isnan(h.getNumber()) || h == 0) continue;
+				if(boost::math::isinf(it->independent().getNumber())) continue;
+				if(boost::math::isinf((it+1)->independent().getNumber())) continue;
+
+				T b1 = it[0].dependent();
+				T b2 = it[1].dependent();
+
+				Trap<T> trap(b1 - offset,b2 - offset,h);
+				max = ot[-1] + trap.getArea();
+				ot[0] = max;
+				cout << max << endl;
+				ot++;
+			}
+		}
+
+		std::vector<T>& getOutput(){return integrated;}
+		//V*nm
+		T getLast(){return max;}
 };
 
 #endif
--- parser2/statsTest.cpp
+++ parser2/statsTest.cpp
@@ -1,10 +1,24 @@
 
 #include "Stats.hpp"
+#include "Measurement-Type/Measurement.hpp"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Integrateable {
+	private:
+		Measurement<double> xvalue, yvalue;
+	public:
+		Integrateable(Measurement<double> x, Measurement<double> y){
+			xvalue = x, yvalue = y;
+		}
+		Measurement<double> independent(){ return xvalue; }
+		Measurement<double> dependent(){ return yvalue; }
+};
+
 int main(){
+	//test the stats library
 	Mean<double> test;
 	for(int i = 0; i < 100; i++) test.push_back(i);
 
@@ -13,4 +27,23 @@ int main(){
 	cout << "Std Dev:     " << test.stdDev() << endl;
 	cout << "Uncertainty: " << test.uncertainty() << endl;
 	cout << "measurement: " << test.measurement() << endl;
+
+
+	//test the trapeziod library
+	Trap<Measurement<double> > testTrap(10,20,4);
+	cout << endl << "Trapezoid Area (b1 = 10, b2 = 20, h = 4): " << testTrap.getArea() << endl;
+
+	//test the integration library
+	vector<Integrateable> testVect;
+	for(int i = 0; i < 40; i++) testVect.emplace_back(i,i*i);
+	Integrate<Integrateable,Measurement<double> > testInt(testVect, Measurement<double>(0));
+	testInt.integration();
+
+	cout << endl << "Integration" << endl;
+	auto intVect = testInt.getOutput();
+	auto ot = testVect.begin();
+	for(auto it = intVect.begin(); it != intVect.end(), ot != testVect.end(); it++, ot++){
+		cout << ot->dependent() << " -> " << *it << endl;
+	}
+	
 }
